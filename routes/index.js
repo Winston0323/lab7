191842d24a93015c78dad60e750cccf4ad0135bb
Winston0323 <529841881@qq.com>
2021-02-18 10:50:00 -0800

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var projects = require('../projects.json');
-
 /*
  * GET home page.
  */
 
 exports.view = function(request, response){
+  	projects["viewAlt"] = false;
   	response.render('index', projects);
 };
+exports.viewAlt = function(request, response){
+  	projects["viewAlt"] = true;
+  	response.render('index', projects);
+};
+
+function initialPage(){
+	$(".likeBtn").click(clickLike);
+}
+function clickLike(){
+	e.preventDefault();
+	ga('create', 'UA-190119141-1', 'auto');
+	ga("send", "event", 'like', 'click');
+}
